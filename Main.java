4c6be45b28552e11efdfea70005943282438dbdf
MAliTamboli <90829419+MAliTamboli@users.noreply.github.com>
2021-09-18 19:58:41 +0530

    Day 2 Assignment

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.arguments;
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[])
+    {
+        int x, y, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        x = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        y = sc.nextInt();
+        sum=x+y;
+        System.out.println("The sum of two numbers x and y is: " +sum);
+
+
+    }
+}
